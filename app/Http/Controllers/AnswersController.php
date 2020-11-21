1d25db986f97630077aaceac774e13cdcceea450
ZYCGary <zyc.gary@gmail.com>
2020-11-21 23:15:42 +1100

    Post Answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 
 class AnswersController extends Controller
 {
-    public function store(Question $question)
+    public function store(Question $questionId)
     {
+        $question = Question::published()->findOrFail($questionId);
         $question->answers()->create([
             'user_id' => request('user_id'),
             'content' => request('content')
--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -6,24 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * App\Models\Answer
- *
- * @property int $id
- * @property int $user_id
- * @property int $question_id
- * @property string $content
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder|Answer newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Answer newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Answer query()
- * @method static \Illuminate\Database\Eloquent\Builder|Answer whereContent($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Answer whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Answer whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Answer whereQuestionId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Answer whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Answer whereUserId($value)
- * @mixin \Eloquent
+ * @mixin IdeHelperAnswer
  */
 class Answer extends Model
 {

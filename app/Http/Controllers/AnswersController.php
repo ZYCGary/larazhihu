44128b07af7e0e9ec38ec7926a817a95710b34bf
ZYCGary <zyc.gary@gmail.com>
2020-11-22 02:12:27 +1100

    Post Answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -7,9 +7,10 @@ use Illuminate\Http\Request;
 
 class AnswersController extends Controller
 {
-    public function store(Question $questionId)
+    public function store($questionId)
     {
         $question = Question::published()->findOrFail($questionId);
+
         $question->answers()->create([
             'user_id' => request('user_id'),
             'content' => request('content')

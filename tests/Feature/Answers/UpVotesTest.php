ab43d6cebea673f274d4b18c46c14385c0b8dda9
ZYCGary <zyc.gary@gmail.com>
2020-11-26 02:22:36 +1100

    vote only once

--- tests/Feature/Answers/UpVotesTest.php
+++ tests/Feature/Answers/UpVotesTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Answers;
 
 use App\Models\Answer;
+use Exception;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -45,5 +46,22 @@ class UpVotesTest extends TestCase
         $this->delete(route('answer-up-votes.destroy', ['answer' => $answer->id]));
 
         $this->assertCount(0, $answer->refresh()->votes('vote_up')->get());
+    }
+
+    /** @test */
+    public function can_vote_up_only_once()
+    {
+        $this->signIn();
+
+        $answer = create(Answer::class);
+
+        try {
+            $this->post(route('answer-up-votes.store', ['answer' => $answer->id]));
+            $this->post(route('answer-up-votes.store', ['answer' => $answer->id]));
+        } catch (Exception $exception) {
+            $this->fail('Cannot vote up twice.');
+        }
+
+        $this->assertCount(1, $answer->refresh()->votes('vote_up')->get());
     }
 }
